e3f154b184732826fefd581d855627fba53710cc
Manuel Moritz-Schliesing <manuel@bitmade.de>
2018-07-02 23:05:53 +0200

    Add Prettier

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ var execPHP = require('exec-php');
 var twigOptions = {
   root: null,
   extensions: [],
-  context: {}
+  context: {},
 };
 
 exports.renderFile = function(entry, options, cb) {
   // Merge the global options with the local ones.
   options = Object.assign({}, twigOptions, options);
 
-  execPHP('php/Twig.php', null, function (error, php) {
+  execPHP('php/Twig.php', null, function(error, php) {
     // Call the callback on error or the render function on success.
-    error ? cb(error) : php.render(entry, options, function (error, stdout) {
-      // Call the callback with an error or the trimmed output.
-      error ? cb(error) : cb(null, stdout.trim());
-    });
+    error
+      ? cb(error)
+      : php.render(entry, options, function(error, stdout) {
+          // Call the callback with an error or the trimmed output.
+          error ? cb(error) : cb(null, stdout.trim());
+        });
   });
 };
 
-exports.createEngine = function (options) {
+exports.createEngine = function(options) {
   // Merge the options with default options.
   twigOptions = Object.assign(twigOptions, options);
 
